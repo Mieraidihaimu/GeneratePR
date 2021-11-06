713c44a17733f0c810de48b6745b19274aa3ed35
Mier Mieraisan <mieraisan@gmail.com>
2021-11-06 14:27:42 +0000

    Fix compiler error :joy

--- Sources/GeneratePR/Helper/CommitLogsFormatter.swift
+++ Sources/GeneratePR/Helper/CommitLogsFormatter.swift
@@ -1,5 +1,5 @@
 //
-//  File.swift
+//  CommitLogsFormatter.swift
 //  
 //
 //  Created by Mier on 06/11/2021.
@@ -9,18 +9,22 @@ import Foundation
 
 struct CommitLogsFormatter {
     private static let linePrefixCharacter = "- "
-    private static let lineBreak = "\n"
+    private static let lineBreak: Character = "\n"
 
     static func format(commitLogsOutput output: String) -> String {
         output
             .split(separator: lineBreak)
-            .map { formatLineIfNeeded($0) }
-            .joined(separator: lineBreak)
+            .map { formatLineIfNeeded(String($0)) }
+            .joined(separator: String(lineBreak))
     }
 
 
     private static func formatLineIfNeeded(_ line: String) -> String {
         guard line.prefix(2) != linePrefixCharacter else { return line }
-        return linePrefixCharacter + line.localizedCapitalized
+        if #available(macOS 10.11, *) {
+            return linePrefixCharacter + line.localizedCapitalized
+        } else {
+            return linePrefixCharacter + line
+        }
     }
 }
